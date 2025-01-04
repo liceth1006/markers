43f5cb65bce661f44d4be0608229610daee9a643
liceth1006 <olmos.1006@gmail.com>
2025-01-04 08:45:15 -0500

    resource

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,12 @@ import Footer from "./components/Footer";
 import Typography from "./pages/Typography.JSX";
 import Templates from "./pages/Templates";
 import DesignTools from "./pages/DesignTools";
-
+import Illustrations from "./pages/Illustrations";
+import Icons from "./pages/Icons";
+import Photos from "./pages/Photos";
+import Textures from "./pages/Textures";
+import Models from "./pages/Models";
+import Backgrounds from "./pages/Backgrounds";
 function App() {
   return (
     <Router>
@@ -31,6 +36,12 @@ function App() {
           <Route path="/color" element={<Color />} />
           <Route path="/typography" element={<Typography />} />
           <Route path="/templates" element={<Templates/>} />
+          <Route path="/backgrounds" element={<Backgrounds/>} />
+          <Route path="/illustrations" element={<Illustrations/>} />
+          <Route path="/icons" element={<Icons/>} />
+          <Route path="/photos" element={<Photos/>} />
+          <Route path="/textures" element={<Textures/>} />
+          <Route path="/3d-models" element={<Models/>} />
           <Route path="/design-tools" element={<DesignTools/>} />
           <Route path="/my-bookmarks" element={<MyBookmarks />} />
         </Routes>
--- src/i18n.js
+++ src/i18n.js
@@ -1,27 +1,40 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
 
 // Configuración de i18next
 i18n
-  .use(HttpApi) 
-  .use(LanguageDetector) 
-  .use(initReactI18next) 
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    fallbackLng: 'en', 
+    fallbackLng: "en",
     debug: true,
     interpolation: {
-      escapeValue: false, 
+      escapeValue: false,
     },
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json', 
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
     },
     react: {
-      useSuspense: false, 
+      useSuspense: false,
     },
-    ns: ['translation','categories', 'department','color','typography','templates','designTools'],  
-    defaultNS: 'translation',  
+    ns: [
+      "translation",
+      "categories",
+      "department",
+      "color",
+      "typography",
+      "templates",
+      "designTools",
+      "illustrations",
+      "icons",
+      "photos",
+      "textures",
+      "backgrounds"
+    ],
+    defaultNS: "translation",
   });
 
 export default i18n;
